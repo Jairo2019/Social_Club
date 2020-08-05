1476e0c40e9b28def053c3917bf0f5bec11bc7d5
Jairo <jairooseguera17@gmail.com>
2020-08-04 21:05:02 -0600

    last commit

--- frntshateam/src/Home/SignIn.js
+++ frntshateam/src/Home/SignIn.js
@@ -37,7 +37,7 @@ export default function Signup({signup, mostrarError}) {
             <div className="FormContainer">
             <h1 className="Form__titulo">ShaTeam</h1>
             <p className="FormContainer__info">
-                Registrate Para explorar tu Hobbys
+                Registrate Para Explorar Hobbys
             </p>
             <form onSubmit={handleInputSubmit}>
                 <input
